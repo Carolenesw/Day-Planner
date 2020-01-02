860687e153cae36cf0a79c8c3734f5d9e4e9efde
Carolene Wilson <carolenesw@gmail.com>
2020-01-02 00:52:08 -0500

    add user input function and onclick to add data

--- planner.js
+++ planner.js
@@ -7,5 +7,22 @@ $(document).ready(function() {
  console.log(toDate);
 $("#date").append(toDate);
  
+//create block to hold user input 
+
+var textArea = $("#text");
+var submit = $("#save")
+ 
+userInputLog();
+
+function displayInfo(type, message) {
+  textArea.textContent = message;
+textArea.setAttribute("class", type); 
+}
+
+("#save").on("click", function(){
+  $("#text").append(submit)
+});
+
+
 
  });

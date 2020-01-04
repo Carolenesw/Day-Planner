740bbdbaa37c906c5319909bb82445eb27399dba
Carolene Wilson <carolenesw@gmail.com>
2020-01-04 09:50:46 -0500

    update js file

--- planner.js
+++ planner.js
@@ -5,39 +5,66 @@ console.log(d);
 
 $("#date").append(d);
 
-
+//add current time slots using moment.js
 var t = moment().format('LT'); 
 console.log(t);
 
 $(".time").append(t);
 
+
+// var hourlyTimeSlot = ["00", "15", "30", "45"];
+// var time = []; 
+// for (var i = 0; i < 24; i++) {
+// for(var j = 0; j < 4; j++) {
+//   time.push(i = ":" + hourlyTimeSlot[j]);
+
+//   console.log("time");
+// }
+// }
+
   //create block to hold user input 
 
   var textArea = $("#text");
-  var submit = $("#save");
+  var submit = $(".save");
 
   var text = localStorage.getItem("text");
 
-  if (userInfo === null) {
+  if (text === null) {
     return;
   }
  
-  textArea.textContext = text;
+  textArea.val(text);
 
 // create click event to show user information when submitted
+// does not work ???
+  $(".save").on("click", function (event) {
 
-  $("#save").on("click", function (event) {
-
-    event.preventDefault();
+    // event.preventDefault();
 
     // textArea.textContext = text;
 
-    var textArea = $("#text").val();
-
+    // var text = $("#text").val();
+    console.log("man");
+    
+    var text = textArea.val();
+console.log(text);
     localStorage.setItem("text", text);
 
-    console.log(event);
+    // console.log(event);
   });
+
+// creat time block color-code  --- giving problem 
+
+function timeSlot() {
+ 
+
+  
+$("#text").css("background-color", "red");
+
+}
+timeSlot();
+
+
 
 
 });

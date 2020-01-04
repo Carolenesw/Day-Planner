9a5139c68440e8557ac628cfbe2d1f55b69ef06a
Carolene Wilson <carolenesw@gmail.com>
2020-01-03 19:43:29 -0500

    add moment.js link

--- planner.js
+++ planner.js
@@ -1,27 +1,40 @@
 $(document).ready(function () {
   //add current date to page
-  var dateToDay = $("#date");
+  
+  // var dateToDay = $("#date");
 
-  var toDate = new Date();
-  toDate.setDate(toDate.getDate() + "");
-  console.log(toDate);
+  // var toDate = new Date();
+  // toDate.setDate(toDate.getDate() + "");
+  // console.log(toDate);
   $("#date").append(toDate);
 
   //create block to hold user input 
 
   var textArea = $("#text");
   var submit = $("#save");
+
+  var text = localStorage.getItem("text");
+
+  if (userInfo === null) {
+    return;
+  }
  
-//create click event to show user information when submitted
+  textArea.textContext = text;
+
+// create click event to show user information when submitted
+
   $("#save").on("click", function (event) {
 
     event.preventDefault();
 
+    // textArea.textContext = text;
+
     var textArea = $("#text").val();
+
+    localStorage.setItem("text", text);
 
     console.log(event);
   });
-
 
 
 });
